02baafb49d9183d519197c50538fea9bd1144530
GabrielStorto <stortogabriel@gmail.com>
2025-12-09 22:58:21 -0400

    estilização do projeto, e inicio da criação do javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+// selecionar elementos
+const root = document.documentElement;
+const btn = document.getElementById("btnTema");
+const ano = document.getElementById("ano")
+
+// preenchendo ano atual
+ano.textContent = new Date().getFullYear()
+
+// tema light/dark
+// btn.addEventListener("click", () => {
+
+// })
